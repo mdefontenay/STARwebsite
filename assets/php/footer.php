d05993afcc88926a072d14a8fb43c9ddd1b84d76
Matthieu de Fontenay <matthieudefontenay@gmail.com>
2023-11-20 00:33:47 -0800

    bug fixing

---
+++ assets/php/footer.php
@@ -0,0 +1,26 @@
+<footer style="margin-top: -45px;">
+    <div class="footer-content">
+        <div id="footer-social">
+            <a href="https://www.facebook.com/ucberkeleystar/" target="_blank"><img src="assets/img/icons/facebook.svg" id="social"></a>
+            <a href="https://www.instagram.com/starberkeley/" target="_blank"><img src="assets/img/icons/instagram.svg" id="social"></a>
+            <a href="https://www.youtube.com/channel/UC-ceqnZp3XVz5mH8297izJA" target="_blank"><img src="assets/img/icons/youtube.svg" id="social"></a>
+            <a href="https://www.linkedin.com/company/calstar" target="_blank"><img src="assets/img/icons/linkedin.svg" id="social"></a>
+            <br>© 2023 by Space Technologies and Rocketry
+        </div>
+        
+        <div id="footer-contact-us">
+            <a href="mailto:contact@stars.berkeley.edu?subject=" class="contact-us" style="font-size: 20pt; width: 150px;">
+                Contact Us
+            </a>
+        </div>
+
+        <div id="footer-join-today">
+            <span style="font-size: 20pt;">Join Today!</span><br>
+            For more information about what STAR is and who we are, check us out at <u><a href="rocketry.gitbook.io">rocketry.gitbook.io</a></u>, and come out to our infosessions at the beginning of each semester! Also, feel free to reach out to any of our team members here and check out the FAQs.
+        </div>
+    </div>
+    <div class="ocf">
+        <a href="https://www.ocf.berkeley.edu/" target="_blank"><img src="assets/img/icons/ocf-hosted-penguin-dark.svg" style="width: 70px;"></a>
+        <span style="margin: auto 0 auto 10px">We are a student group acting independently of the University of California. We take full responsibility for our organization and this website.</span>
+    </div>
+</footer>
